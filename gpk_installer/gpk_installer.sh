58c11c342880872ff0408739cffca7996bba4f49
librehat <hzwhuang@gmail.com>
2012-05-25 22:03:29 +0800

    GPK Installer init.

---
+++ gpk_installer/gpk_installer.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+ver="0.1.0"
+#Author William Wong (@librehat)
+#License: GPLv3
+file=$1
+
+function extract
+{
+	if [ -d out ] && rm -rf out;then
+		echo "Extracting..."
+		if unzip -q $file -d out;then
+			iapk && pushdata
+		else
+			echo "Extract failed."
+			exit 1
+		fi
+	else
+		echo "Abort: Remove out directory failed."
+		exit 1
+	fi
+}
+
+function iapk
+{
+	echo "Installing..."
+	adb install out/application.apk
+}
+
+function pushdata
+{
+	echo "Copying data files..."
+	dataname=`ls out/ | grep "com."`
+	if [ -n "$dataname" ];then
+		adb push out/$dataname /mnt/sdcard/Android/data/$dataname
+	else
+		dataname=`ls out/ | grep Gloft`
+		if [ -n "$dataname" ];then
+			adb push out/$dataname /mnt/sdcard/gameloft/$dataname
+		else
+			echo "ERROR: Couldn't recognize the type of data."
+			echo "You need to copy the data files manually."
+			exit 1
+		fi
+	fi
+}
+
+echo "GPK Installer. Version: $ver"
+
+if [ -n "$file" ];then
+	extract
+else
+	echo "ERROR: No input file."
+	echo
+	echo "Usage:"
+	echo "./extract_gpk.sh [gpk filename]"
+	echo "Be patient while extracting, installing and moving."
+	echo "i.e.:"
+	echo "./extract_gpk.sh a_game.gpk"
+	echo
+fi
